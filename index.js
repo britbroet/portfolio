674b82c9d4c8b702db3c3fc8faac77922881b1e6
britbroet <brittanybroetje@gmail.com>
2016-12-15 11:46:40 -0800

    header fixes

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ app.get('/*', function(req, res) {
   res.sendFile(__dirname + '/public/index.html');
 });
 
-app.listen(process.env.PORT || 3000)
+var server = app.listen(process.env.PORT || 3000);
--- public/js/app/app.js
+++ public/js/app/app.js
@@ -1,7 +1,6 @@
 angular.module('PortfolioApp', ['ui.router','ui.bootstrap','ngMaterial','ngMessages','material.svgAssetsCache'])
 .config(['$stateProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $urlRouterProvider, $locationProvider){
 	$urlRouterProvider.otherwise('/'); // usually goes to a 404 route
-
 	$stateProvider
 	.state('home', {
 		url: '/home',
