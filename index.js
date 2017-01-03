9c1a5e5257d1c03f4dd33f9821f889ea1c572159
britbroet <brittanybroetje@gmail.com>
2017-01-03 11:41:09 -0800

    reset index.html and js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(__dirname + '/public/'));
+app.use(express.static(__dirname + '/public'));
 
 
 app.get('/*', function(req, res) {
   console.log("sending index");
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 var server = app.listen(process.env.PORT || 3000);
